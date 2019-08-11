44295d0e395088b1272371f2fcbd6ac97e9320ec
Fullmetal235 <jjbillupsbrown@gmail.com>
2019-08-11 18:15:12 -0500

    portfolio wIP

---
+++ js/betterpro.js
@@ -0,0 +1,31 @@
+const signupbtn = document.querySelector("#btn1");
+signupbtn.addEventListener('mouseleave', event =>{
+  event.target.style.background = 'black';
+});
+
+const signupbtn2 = document.querySelector("#btn1");
+signupbtn2.addEventListener('mouseenter', event =>{
+  event.target.style.background = 'dodgerblue';
+});
+
+
+const upperButton1 = document.querySelector("#up_btn")
+upperButton1.addEventListener('mouseleave', event =>{
+  event.target.style.background = 'dodgerblue';
+});
+
+const upperButton2 = document.querySelector("#up_btn");
+upperButton2.addEventListener('mouseenter', event =>{
+  event.target.style.background = 'black';
+});
+
+
+const upperButton3 = document.querySelector("#up_btn2")
+upperButton3.addEventListener('mouseleave', event =>{
+  event.target.style.background = 'dodgerblue';
+});
+
+const upperButton4 = document.querySelector("#up_btn2"); 
+upperButton4.addEventListener('mouseenter', event =>{
+  event.target.style.background = 'black';
+});
---
+++ js/betterproabout.js
@@ -0,0 +1,20 @@
+const upperButton1 = document.querySelector("#up_btn")
+upperButton1.addEventListener('mouseleave', event =>{
+  event.target.style.background = 'dodgerblue';
+});
+
+const upperButton2 = document.querySelector("#up_btn");
+upperButton2.addEventListener('mouseenter', event =>{
+  event.target.style.background = 'black';
+});
+
+
+const upperButton3 = document.querySelector("#up_btn2")
+upperButton3.addEventListener('mouseleave', event =>{
+  event.target.style.background = 'dodgerblue';
+});
+
+const upperButton4 = document.querySelector("#up_btn2"); 
+upperButton4.addEventListener('mouseenter', event =>{
+  event.target.style.background = 'black';
+});
